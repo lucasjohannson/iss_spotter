efdcf39b25722d251c9ee6d807b870562b8175bc
lucas <lucas.johannson1@ucalgary.ca>
2021-06-03 23:10:26 +0000

    using promises

---
+++ index2.js
@@ -0,0 +1,8 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+// index2.js
+
+// index2.js
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
